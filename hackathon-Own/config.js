a8a4f162933a1d9fcffc9926c5319427fe69aca4
ZARNAIN-NAZ <zarnainabid004@gmail.com>
2023-09-10 01:42:10 +0500

    singup and login page sucessfully created and connected with firebase

---
+++ hackathon-Own/config.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCphnVP3Kle7ALT5_NXhaGFxalueN3_2O4",
+  authDomain: "hackathon-prac-9f6c8.firebaseapp.com",
+  projectId: "hackathon-prac-9f6c8",
+  storageBucket: "hackathon-prac-9f6c8.appspot.com",
+  messagingSenderId: "351946178563",
+  appId: "1:351946178563:web:638b265ac8372b91a6ce36",
+  measurementId: "G-J6DW1QBBYJ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
